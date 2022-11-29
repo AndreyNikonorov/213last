147ee70e3c8b407bc3de04cf61145b85195d0d66
Andrei Nikonorov <nikorova@mail.ru>
2022-11-29 14:25:13 +0300

    finished

--- PP_2_1_3_SpringBeans-master/src/main/java/app/model/AnimalsCage.java
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/AnimalsCage.java
@@ -8,13 +8,20 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier("cat")
+    @Qualifier("dog")
     private Animal animal;
 
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer(){
+        return timer;
+    }
+
     public void whatAnimalSay() {
-        System.out.println("Say:");
+        System.out.print("Say:");
         System.out.println(animal.toString());
-        System.out.println("At:");
+        System.out.print("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
